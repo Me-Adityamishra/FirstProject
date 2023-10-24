bcf0d2deba272b8689bdb9e6cfb6d7fd03a04ea2
Aditya <adityakumar03569@gmail.com>
2023-10-24 18:54:38 +0530

    first commit

---
+++ add.java
@@ -0,0 +1,11 @@
+public class add
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hi aditya");
+        System.out.println("hi aditya");
+        System.out.println("hi aditya");
+        System.out.println("hi aditya");
+                
+    }
+}
